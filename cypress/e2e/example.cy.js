ff327223f1664a50c818cd0deff1532af05fdc1e
Henrique Fontenele <henrique.fontenele@clicksign.com>
2024-11-12 11:18:13 -0300

    Update example.cy.js

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -3,6 +3,6 @@ import { qase } from 'cypress-qase-reporter/mocha';
 describe('Suite Henrique', () => {
   qase(503,
     it('Caso de Teste 3 - Henrique via Cypress', () => {
-      expect(true).to.equal(true);
+      expect(true).to.equal(false);
     }));
 });
